f7e224f19b2977ccd6ef4a0f0ccff8f3568db535
jrhizor <me@jaredrhizor.com>
2022-11-27 16:44:30 -0800

    try again

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   url: 'https://docs.stoat.dev/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenMarkdownLinks: 'throw',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
