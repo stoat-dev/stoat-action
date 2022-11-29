81875ac1b7e7013f31cbc97afa3015eb18b82455
jrhizor <me@jaredrhizor.com>
2022-11-29 13:07:09 -0800

    Revert "use correct env var to determine prod"
    This reverts commit abe1db54bf47942366da30ccb6e125b5f5e5295b.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -131,9 +131,9 @@ const config = {
     })
 };
 
-// Uses NEXT_PUBLIC_VERCEL_ENV instead of NODE_ENV, so we can exclude previews from analytics collection.
+// Uses VERCEL_ENV instead of NODE_ENV, so we can exclude previews from analytics collection.
 // see https://vercel.com/docs/concepts/projects/environment-variables#system-environment-variables
-const enableAnalytics = process.env.NEXT_PUBLIC_VERCEL_ENV === 'production';
+const enableAnalytics = process.env.VERCEL_ENV === 'production';
 
 if (enableAnalytics) {
   config['scripts'] = [
