722c3164e06df868d91f32c3080c5a731a03643e
jrhizor <me@jaredrhizor.com>
2022-11-28 17:41:43 -0800

    redirect home page to intro page

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,4 +1,5 @@
 import Link from '@docusaurus/Link';
+import { Redirect } from '@docusaurus/router';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Layout from '@theme/Layout';
@@ -7,29 +8,23 @@ import React from 'react';
 
 import styles from './index.module.css';
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/intro">
-            Get Started!
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+// function HomepageHeader() {
+//   const { siteConfig } = useDocusaurusContext();
+//   return (
+//     <header className={clsx('hero hero--primary', styles.heroBanner)}>
+//       <div className="container">
+//         <h1 className="hero__title">{siteConfig.title}</h1>
+//         <p className="hero__subtitle">{siteConfig.tagline}</p>
+//         <div className={styles.buttons}>
+//           <Link className="button button--secondary button--lg" to="/docs/intro">
+//             Get Started!
+//           </Link>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>{/*<HomepageFeatures />*/}</main>
-    </Layout>
-  );
+  return <Redirect to="/docs/intro" />;
 }
