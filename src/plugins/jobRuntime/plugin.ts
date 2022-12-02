277f381105b7ed424e36bda5bb1d08b610553629
Liren Tu <tuliren@gmail.com>
2022-12-01 16:09:18 -0800

    Submit partial config for job runtime

--- src/plugins/jobRuntime/plugin.ts
+++ src/plugins/jobRuntime/plugin.ts
@@ -1,6 +1,7 @@
 import * as core from '@actions/core';
 
 import { GithubActionRun, UploadJobRuntimeRequest } from '../../types';
+import { submitPartialConfig } from '../helpers';
 
 const runJobRuntimePlugin = async (
   taskId: string,
@@ -10,16 +11,17 @@ const runJobRuntimePlugin = async (
   core.info(`[${taskId}] Running static hosting plugin (stoat config ${stoatConfigFileId})`);
 
   for (const ghJob of ghJobs) {
-    core.info(`[${taskId}] Uploading job runtime for ${ghJob.name}: ${JSON.stringify(ghJob)}`);
+    const runtimeSeconds = Math.floor(new Date().valueOf() - new Date(ghJob.started_at).valueOf()) / 1000;
+    core.info(`[${taskId}] Uploading job runtime for ${ghJob.name}: ${runtimeSeconds}`);
     const requestBody: UploadJobRuntimeRequest = {
       ghSha,
       taskId,
       stoatConfigFileId,
       ghToken,
       ghJobName: ghJob.name,
-      runtimeSeconds: 0
+      runtimeSeconds
     };
-    // await submitPartialConfig<UploadJobRuntimeRequest>(taskId, 'job_runtimes', requestBody);
+    await submitPartialConfig<UploadJobRuntimeRequest>(taskId, 'job_runtimes', requestBody);
   }
 };
 
