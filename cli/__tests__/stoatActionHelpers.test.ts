09a3f572ffbe96f6f9a6f0875f791e0a00611921
jrhizor <me@jaredrhizor.com>
2022-12-04 23:21:58 -0800

    add other test cases

--- cli/__tests__/stoatActionHelpers.test.ts
+++ cli/__tests__/stoatActionHelpers.test.ts
@@ -5,9 +5,21 @@ import path from 'path';
 import { addStoatActionToYaml } from '../src/lib/init/configFileHelpers';
 
 describe('Add Stoat Action to YAML', () => {
-  test('addStoatActionToYaml', async () => {
+  test('add action to build job', async () => {
     expect(addStoatActionToYaml({ name: 'build', workflowFile: path.join(__dirname, 'example.yaml') })).toEqual(
-      fs.readFileSync(path.join(__dirname, 'expected-added-build.yaml')).toString()
+      fs.readFileSync(path.join(__dirname, 'expected-add-build.yaml')).toString()
     );
+  });
+
+  test('add action to build2 job', async () => {
+    expect(addStoatActionToYaml({ name: 'build2', workflowFile: path.join(__dirname, 'example.yaml') })).toEqual(
+      fs.readFileSync(path.join(__dirname, 'expected-add-build2.yaml')).toString()
+    );
+  });
+
+  test('add action to both build and build2 jobs', async () => {
+    expect(
+      addStoatActionToYaml({ name: 'build2', workflowFile: path.join(__dirname, 'expected-add-build.yaml') })
+    ).toEqual(fs.readFileSync(path.join(__dirname, 'expected-add-build-and-build2.yaml')).toString());
   });
 });
