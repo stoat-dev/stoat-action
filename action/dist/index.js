bc07899055b97493f9158651b74772bbe64059c0
Liren Tu <tuliren@gmail.com>
2022-12-08 15:57:09 -0800

    Update package

--- action/dist/index.js
+++ action/dist/index.js
@@ -24350,7 +24350,7 @@ const getApiUrlBase = (ghOwner, ghRepo) => __awaiter(void 0, void 0, void 0, fun
     catch (e) {
         core.warning(`Testing connection to "${devApiUrlBase}" failed: ${e}`);
     }
-    core.warning(`Fall back from "${devApiUrlBase}" to ${PROD_API_URL_BASE}`);
+    core.warning(`Fall back to ${PROD_API_URL_BASE}`);
     return PROD_API_URL_BASE;
 });
 /**
