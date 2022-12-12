492f84927bada5109dad5951c02b3da3d94d604c
Liren Tu <tuliren@gmail.com>
2022-12-12 12:53:22 -0800

    Run job runtime plugin by default

--- action/src/plugins/pluginRunner.ts
+++ action/src/plugins/pluginRunner.ts
@@ -11,15 +11,21 @@ export const runPlugins = async (
   githubActionRun: GithubActionRun,
   stoatConfigFileId: number
 ): Promise<void> => {
+  let hasJobRuntime = false;
   for (const [taskId, taskConfig] of Object.entries(stoatConfig.tasks || {})) {
     if ('static_hosting' in taskConfig) {
       await runStaticHostingPlugin(taskId, taskConfig as StaticHostingPlugin, githubActionRun, stoatConfigFileId);
     } else if ('json' in taskConfig) {
       await runJsonPlugin(taskId, taskConfig as JsonPlugin, githubActionRun, stoatConfigFileId);
     } else if ('job_runtime' in taskConfig) {
+      hasJobRuntime = true;
       await runJobRuntimePlugin(taskId, taskConfig as JobRuntimePlugin, githubActionRun, stoatConfigFileId);
     } else {
       core.warning(`Unknown plugin: ${taskId}`);
     }
+  }
+  if (!hasJobRuntime) {
+    const defaultJobRuntimeConfig: JobRuntimePlugin = { job_runtime: {} };
+    await runJobRuntimePlugin('default-job-runtime', defaultJobRuntimeConfig, githubActionRun, stoatConfigFileId);
   }
 };
